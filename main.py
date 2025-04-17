37a60cc0ca086b72921313f12e7708a09b684010
Mossblac <Murphyrmurphy2000@gmail.com>
2025-04-17 15:36:05 -0600

    point and line classes added, lines created

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import tkinter as tk
-import time
-from tkinter import Tk, BOTH, Canvas
-from window import Window
+
+from window import *
+from points_and_lines import *
+
 
 def main():
 
     
 
     display = Window(1280, 720)
+    display.draw_line(line1, "red")
+    display.draw_line(line2, "black")
     display.wait_for_close()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -4,6 +4,7 @@ from tkinter import Tk, BOTH, Canvas
 
 
 
+
 class Window:
         def __init__(self, width, height):
             self.root = tk.Tk()
@@ -25,3 +26,20 @@ class Window:
 
         def close(self):
             self.is_running = False
+
+        def draw_line(self, line, fill_color):
+             line.draw(self.canvas, fill_color=fill_color)
+
+class Point:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+class Line:
+        def __init__(self, point1, point2):
+            self.p1 = point1
+            self.p2 = point2
+
+        def draw(self, canvas, fill_color):
+             canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+            
