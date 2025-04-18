eef5fa13277f2984be868ce65f5b92ba604c59d1
Mossblac <Murphyrmurphy2000@gmail.com>
2025-04-18 07:46:52 -0600

    resized window and starter cells

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from window import *
 def main():
     
 
-    display = Window(1280, 720)
-    cell_start = Cell(10, 10, 110, 110, display, True, False, True, True)
-    cell2 = Cell(110, 10, 210, 110 , display, False, False, True, True)
+    display = Window(800, 800)
+    cell_start = Cell(10, 10, 60, 60, display, True, False, True, True)
+    cell2 = Cell(60, 10, 110, 60 , display, False, False, True, True)
     cell_start.draw()
     cell2.draw()
     cell_start.draw_move(cell2, False)
